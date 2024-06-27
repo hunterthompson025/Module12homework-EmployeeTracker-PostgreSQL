d906481c5c333334bcdbc84071620adba5dd4741
Hunter Thompson <hunterthompson025@gmail.com>
2024-06-27 10:04:15 -0700

    This commit creates the logic for the addEmployee function, and modifies the seeds file to have less data.

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,29 +7,17 @@ VALUES
 INSERT INTO role (id, title, salary, department_id)
 VALUES
 (1, 'Developer', 100000, 1),
-(2, 'Sr Developer', 150000, 1),
-(3, 'Lead Developer', 200000, 1),
-(4, 'Development Manager', 250000, 1),
-(5, 'Analyst', 50000, 2),
-(6, 'Sr Analyst', 65000, 2),
-(7, 'Lead Analyst', 75000, 2),
-(8, 'QA Manager', 100000, 2),
-(9, 'Representative', 45000, 3),
-(10, 'Sr Representative', 55000, 3),
-(11, 'Lead Representative', 70000, 3),
-(12, 'CS Manager', 95000, 3);
+(2, 'Development Manager', 250000, 1),
+(3, 'Analyst', 50000, 2),
+(4, 'QA Manager', 100000, 2),
+(5, 'Representative', 45000, 3),
+(6, 'CS Manager', 95000, 3);
 
 INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
 VALUES
-(1, 'Luke', 'Lockard', 1, 4),
-(2, 'Jake', 'Roy', 2, 4),
-(3, 'Rylee', 'Contois', 3, 4),
-(4, 'Garrett', 'Blythe', 4, null),
-(5, 'Kenny', 'Davis', 5, 8),
-(6, 'Mike', 'Sexton', 6, 8),
-(7, 'Hunter', 'Thompson', 7, 8),
-(8, 'Stephanie', 'Harrigan', 8, null),
-(9, 'Brycen', 'Head', 9, 12),
-(10, 'Michael', 'Brandt', 10, 12),
-(11, 'Elisa', 'Moraga', 11, 12),
-(12, 'Pam', 'Moore', 12, null);
+(1, 'Luke', 'Lockard', 1, 2),
+(2, 'Garrett', 'Blythe', 2, null),
+(3, 'Kenny', 'Davis', 3, 4),
+(4, 'Stephanie', 'Harrigan', 4, null),
+(5, 'Brycen', 'Head', 5, 6),
+(6, 'Pam', 'Moore', 6, null);
--- server.js
+++ server.js
@@ -22,10 +22,23 @@ const viewDepartments = async () => {
   try {
     const query = 'SELECT *  FROM department';
     const res = await pool.query(query);
-    console.log('View All Departments selected. Departments:', res.rows);
+    console.log('View All Departments');
+    console.table(res.rows);
     promptUser();
   } catch (err) {
     console.error('Error Viewing Departments', err);
+  }
+};
+
+const viewRoles = async() => {
+  try{
+    const query = 'SELECT * FROM role';
+    const res = await pool.query(query);
+    console.log('View all Roles');
+    console.table(res.rows);
+    promptUser();
+  } catch (err) {
+    console.error('Error Viewing Roles', err);
   }
 };
 
@@ -52,7 +65,7 @@ const addDepartment = async () => {
     console.log(`Added ${name} to the database`);
     promptUser();
   } catch (err) {
-    console.error('Error Adding Department', err);
+    console.error('Error adding Department', err);
   }
 };
 
@@ -88,7 +101,7 @@ const addRole = async () => {
     console.log(`Department ID for ${department_name} is ${departmentId}`);
 
     console.log(departmentId);
-    
+
     if (!departmentId) {
       console.log(departmentId);
       console.error('Department does not exist. Please try again.');
@@ -98,9 +111,9 @@ const addRole = async () => {
     const maxIdQuery = 'SELECT MAX(id) as max_id FROM role';
     console.log('max query', maxIdQuery);
     const maxIdResult = await pool.query(maxIdQuery);
-    console.log('max id',maxIdResult);
+    console.log('max id', maxIdResult);
     const nextId = (maxIdResult.rows[0].max_id || 0) + 1;
-    console.log('next id',nextId);
+    console.log('next id', nextId);
     const query = 'INSERT INTO role (id, title, salary, department_id) VALUES ($1, $2, $3, $4) RETURNING *';
     const res = await pool.query(query, [nextId, title, salary, departmentId]);
     console.log(`Added ${title} to the ${department_name} department in the database`);
@@ -110,8 +123,86 @@ const addRole = async () => {
   }
 };
 
-const addEmployee = () => {
+const addEmployee = async () => {
+  try {
+    const { first_name, last_name, role_title, manager_name } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: `What is the employee's first name?`,
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: `What is the employee's last name?`,
+      },
+      {
+        type: 'input',
+        name: 'role_title',
+        message: `What is the employee's role?`,
+      },
+      {
+        type: 'input',
+        name: 'manager_name',
+        message: `Who is the employee's manager?`,
+      },
+    ]);
 
+    const roleQuery = 'SELECT id FROM role WHERE title = $1';
+    const roleResult = await pool.query(roleQuery, [role_title]);
+
+    console.log('Role query result:', roleResult);
+
+    const roleId = roleResult.rows[0].id;
+    console.log(`Role ID for ${role_title} is ${roleId}`);
+
+    console.log('role id', roleId);
+
+    if (!roleId) {
+      console.log(roleId);
+      console.error('Role does not exist. Please try again');
+      return;
+    }
+
+    const [manager_first_name, manager_last_name] = manager_name.split(' ');
+    console.log('manager_first_name', manager_first_name);
+    console.log('manager_last_name', manager_last_name);
+
+    if (!manager_first_name || !manager_last_name) {
+      console.error('Invalid Manager Name');
+      return;
+    }
+
+    const managerQuery = 'SELECT id FROM employee WHERE first_name = $1 AND last_name = $2';
+    const managerResult = await pool.query(managerQuery, [manager_first_name, manager_last_name]);
+
+    console.log('managerResult', managerResult);
+
+    const managerId = managerResult.rows[0]?.id;
+
+    console.log('managerId', managerId);
+
+
+    if (!managerId) {
+      console.error('Invalid Manager Name. Please try again');
+      return;
+    }
+
+    //find the highest employee id and create the new employee with the the id+1
+    const maxIdQuery = 'SELECT MAX(id) as max_id FROM employee';
+    console.log('max query', maxIdQuery);
+    const maxIdResult = await pool.query(maxIdQuery);
+    console.log('max id', maxIdResult);
+    const nextId = (maxIdResult.rows[0].max_id || 0) + 1;
+    console.log('next id', nextId);
+
+    const query = 'INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+    const res = await pool.query(query, [nextId, first_name, last_name, roleId, managerId]);
+    console.log(`Added ${first_name} ${last_name} to the database`)
+    promptUser();
+  } catch (err) {
+    console.error('Error adding Employee', err.message);
+  }
 };
 
 const updateEmployeeRole = () => {
@@ -124,7 +215,7 @@ const promptUser = () => {
       type: 'list',
       name: 'prompt',
       message: 'What would you like to do?',
-      choices: ['View All Departments', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role'],
+      choices: ['View All Departments', 'View All Employees', 'View All Roles', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role'],
     },
   ]).then((answers) => {
     console.log('User selected:', answers.prompt);
@@ -135,13 +226,16 @@ const promptUser = () => {
       case 'View All Employees':
         viewEmployees();
         break;
+      case 'View All Roles':
+        viewRoles();
+        break;
       case 'Add Department':
         addDepartment();
         break;
       case 'Add Role':
         addRole();
         break;
-      case 'Add Emplyoee':
+      case 'Add Employee':
         addEmployee();
         break;
       case 'Update Employee Role':
